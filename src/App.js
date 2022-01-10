2d8b90ef66d1384418f741e36f51e20b4756532f
thayna-saad <thaynasaad.ts@gmail.com>
2022-01-10 17:29:13 -0300

    Tela Login - Criada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Header } from './Header/Header';
+import Entrar from './páginas/Entrar/Entrar';
 
 const App = () => {
   return(
     <div>
-      <Header/>
+      <Entrar/>
     </div>
   )
 }
--- src/páginas/Entrar/estilo.js
+++ src/páginas/Entrar/estilo.js
@@ -3,18 +3,71 @@ import styled from "styled-components"
 export const ContainerPai = styled.div `
     display: flex;
     flex-direction: column;
-    align-items:center;
-    width: 100vw;
-    justify-content:center;
+    align-items: center;
+    width: 100%;
+    
+    h1 {
+        width: 296px;
+        height: 18px;
+        font-family: Roboto;
+        font-size: 20px;
+        font-weight: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.39px;
+        text-align: center;
+        color: var(--black);
+    }
 
 `
 
-export const InputsContainer = styled.div`
-    display: flex;
-    flex-direction:column;
-    width: 80vw;
-    max-width:450px;
-    align-items:center;
-    margin-bottom:20px;
+export const InputRetangulo = styled.div `
+    width: 328px;
+    height: 56px;
+    margin: 8px 0 0;
+    padding: 19px 48px 19px 16px;
+    border-radius: 2px;
+    border:solid 1px #b8b8b8;
+    border-radius: 8px;
 
+    span {
+        width: 78px;
+        height: 18px;
+        margin: 3px 234px 43px 16px;
+        font-family: Roboto;
+        font-size: 12px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.29px;
+        color: #b8b8b8;
+    
+    }
+    
+
+    input {
+        width: 264px;
+        height: 18px;
+        font-family: Roboto;
+        font-size: 16px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.50px;
+        color: #d0d0d0;
+    }
+`
+
+export const ButtonLogin = styled.div `
+    button {
+        margin: 10px;
+        width: 328px;
+        height: 42px;
+        padding: 12px 16px;
+        border-radius: 5px;
+        background-color: #5cb646;
+    }
+    
 `

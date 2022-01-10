c47da61d913f0ca42406d65310a19a046c475cdd
desatinar <rodrigosdn@gmail.com>
2022-01-10 17:10:05 -0300

    ajeitado o conflito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Header } from './Header/Header';
-import Perfil from './páginas/Perfil/Perfil';
 
 const App = () => {
   return(
     <div>
-      <Perfil/>
+      <Header/>
     </div>
   )
 }

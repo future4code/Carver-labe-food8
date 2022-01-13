7872c0e1ba83b8977377a95430d031533dd0df38
Thamires Lippelt Vieira <thamivieira@gmail.com>
2022-01-13 16:32:28 -0300

    tentando fazer as funções funcionarem

--- src/componentes/CardRestaurante/CardRestaurante.js
+++ src/componentes/CardRestaurante/CardRestaurante.js
@@ -3,23 +3,23 @@ import EstadoGlobal from "../../EstadoGlobal/EstadoGlobal";
 import { BotaoAdd, BotaoRem, CardDetalhes, ContainerCard, Detalhes, ImgProduto, NomeDoItem, Preco, TextDescritivo } from "./estilo";
 
 const CardRestaurante = (props, carrinho, setCarrinho) => {
-    const { carrinho, setCarrinho } = useContext(EstadoGlobal)
+    // const { carrinho, setCarrinho } = useContext(EstadoGlobal)
 
-    const addCarrinho = (addItem) => {
-        const indexProd = props.detalhe.findIndex((i) => i.id === addItem.id);
-        const novoCarrinho = [...carrinho];
+    // const addCarrinho = (addItem) => {
+    //     const indexProd = props.detalhe.findIndex((i) => i.id === addItem.id);
+    //     const novoCarrinho = [...carrinho];
 
-        if (indexProd === -1) {
-            // não está no carrinho
-            const carrItem = { ...addItem, amount: 1 };
-            novoCarrinho.push(carrItem);
-        } else {
-            // já está no carrinho
-            novoCarrinho[indexProd].amount += 1;
-        }
+    //     if (indexProd === -1) {
+    //         // não está no carrinho
+    //         const carrItem = { ...addItem, amount: 1 };
+    //         novoCarrinho.push(carrItem);
+    //     } else {
+    //         // já está no carrinho
+    //         novoCarrinho[indexProd].amount += 1;
+    //     }
 
-        setCarrinho(novoCarrinho);
-    };
+    //     setCarrinho(novoCarrinho);
+    // };
 
     // const productCards =
     //     products &&
@@ -33,33 +33,33 @@ const CardRestaurante = (props, carrinho, setCarrinho) => {
     //         );
     //     });
 
-    const remCarrinho = (remItem) => {
-        const indexProd = props.detalhe.findIndex((i) => i.id === remItem.id)
-        const novoCarrinho = [...carrinho]
-        if (novoCarrinho[indexProd].amount === 1) {
-            // Se só tem um, quero remover
-            novoCarrinho.splice(indexProd, 1);
-        } else {
-            // Se tem mais de um, quero diminuir
-            novoCarrinho[indexProd].amount -= 1;
-        }
-        setCarrinho(novoCarrinho);
-    };
+    // const remCarrinho = (remItem) => {
+    //     const indexProd = props.detalhe.findIndex((i) => i.id === remItem.id)
+    //     const novoCarrinho = [...carrinho]
+    //     if (novoCarrinho[indexProd].amount === 1) {
+    //         // Se só tem um, quero remover
+    //         novoCarrinho.splice(indexProd, 1);
+    //     } else {
+    //         // Se tem mais de um, quero diminuir
+    //         novoCarrinho[indexProd].amount -= 1;
+    //     }
+    //     setCarrinho(novoCarrinho);
+    // };
 
-    console.log(novoCarrinho)
+    // console.log(novoCarrinho)
 
 
     const mudaBotao = () => {
 
         if (props.detalhe.where === 'restaurante') {
             return (
-                <BotaoAdd onClick={addCarrinho}>
+                <BotaoAdd >
                     ADICIONAR
                 </BotaoAdd>
             );
         } else {
             return (
-                <BotaoRem onClick={remCarrinho}>
+                <BotaoRem >
                     REMOVER
                 </BotaoRem>
             );
